3d63c278a0fafe34017429320b1df794fe2e5b66
Zak Shearman <shearmanzak@gmail.com>
2020-10-30 15:46:26 +0000

    add known nav links

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -7,9 +7,9 @@ const NavBar = ({}) => {
     <nav className={styles.navbar}>
       <Link href="/"><img src="/icon-64.png" alt="logo" /></Link>
       <Link href="/donate"><a>Donate</a></Link>
-      <Link href="/support"><a>Support</a></Link>
+      <Link href="https://discord.gg/aS4PebKZpe"><a>Support</a></Link>
       <Link href="/docs"><a>Docs</a></Link>
-      <Link href="/github"><a>GitHub</a></Link>
+      <Link href="https://github.com/SmartGiveaways"><a>GitHub</a></Link>
       <div className={styles.discover_coming_soon}>
         <Link href="/"><a>Discover</a></Link>
         <span className={styles.soon_badge}>Soon</span>
